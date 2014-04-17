f0183268c66d9ca57aafa74ce1c644d64eb015bd
Tae Won Ha <qvacua@gmail.com>
2014-04-17 19:11:23 +0200

    #5 add a macro which converts NSString to CPP string

--- VimR/VRUtils.h
+++ VimR/VRUtils.h
@@ -1,1 +1,7 @@
-#define SF(format, ...) [NSString stringWithFormat:format, ##__VA_ARGS__]
+#ifndef SF
+    #define SF(format, ...) [NSString stringWithFormat:format, ##__VA_ARGS__]
+#endif
+
+#ifndef CPP_STR
+    #define CPP_STR(str) cf::to_s((__bridge CFStringRef) str)
+#endif
