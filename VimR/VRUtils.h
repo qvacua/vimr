2b2885827623d550d2308d070ec5d37879229233
Tae Won Ha <qvacua@gmail.com>
2014-05-17 22:15:30 +0200

    #3 minor changes

--- VimR/VRUtils.h
+++ VimR/VRUtils.h
@@ -14,7 +14,7 @@ OBJC_EXTERN inline double measure_time(dispatch_block_t block);
 OBJC_EXTERN inline void dispatch_to_main_thread(dispatch_block_t block);
 OBJC_EXTERN inline void dispatch_to_global_queue(dispatch_block_t block);
 OBJC_EXTERN inline void dispatch_loop(size_t count, void (^block)(size_t));
-OBJC_EXTERN NSURL *common_parent_url(NSArray *fileUrls);
+OBJC_EXTERN inline NSURL *common_parent_url(NSArray *fileUrls);
 OBJC_EXTERN inline NSValue *vsize(CGSize size);
 OBJC_EXTERN inline NSValue *vrect(CGRect rect);
 OBJC_EXTERN inline NSValue *vpoint(CGPoint point);
