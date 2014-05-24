f8a6350ec31b2f03de702c89e8b06deec4e049ec
Tae Won Ha <qvacua@gmail.com>
2014-05-24 21:58:35 +0200

    #21 delete measure time

--- VimR/VRUtils.m
+++ VimR/VRUtils.m
@@ -1,14 +1,6 @@
 #import "VRUtils.h"
 
 
-double measure_time(dispatch_block_t block) {
-  clock_t tic = clock();
-  block();
-  clock_t toc = clock();
-
-  return (double) (toc - tic) / CLOCKS_PER_SEC;
-}
-
 void dispatch_to_main_thread(dispatch_block_t block) {
   dispatch_async(dispatch_get_main_queue(), block);
 }
