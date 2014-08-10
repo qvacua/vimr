a29cd5efe6cb113ad77d7a810286d6139f98a9d3
Tae Won Ha <qvacua@me.com>
2014-08-10 15:37:01 +0200

    #82 add test for select open url

--- VimRTests/VRWorkspaceTest.m
+++ VimRTests/VRWorkspaceTest.m
@@ -1,15 +1,16 @@
 /**
- * Tae Won Ha — @hataewon
- *
- * http://taewon.de
- * http://qvacua.com
- *
- * See LICENSE
- */
+* Tae Won Ha — @hataewon
+*
+* http://taewon.de
+* http://qvacua.com
+*
+* See LICENSE
+*/
 
 #import "VRBaseTestCase.h"
 #import "VRWorkspace.h"
 #import "VRMainWindowController.h"
+#import "VRMainWindowControllerFactory.h"
 
 
 @interface VRWorkspaceTest : VRBaseTestCase
@@ -19,27 +20,49 @@
   VRWorkspace *workspace;
 
   MMVimController *vimController;
+  NSWindow *window;
   VRMainWindowController *mainWindowController;
+  VRMainWindowControllerFactory *mainWindowControllerFactory;
 }
 
 - (void)setUp {
   [super setUp];
 
+  workspace = [[VRWorkspace alloc] init];
+
   vimController = mock([MMVimController class]);
+
+  window = mock([NSWindow class]);
   mainWindowController = mock([VRMainWindowController class]);
+  mainWindowControllerFactory = mock([VRMainWindowControllerFactory class]);
+  [[given([mainWindowControllerFactory newMainWindowControllerWithContentRect:CGRectZero
+                                                                    workspace:workspace
+                                                                vimController:vimController])
+      withMatcher:anything() forArgument:0]
+      willReturn:mainWindowController];
 
   [given([mainWindowController vimController]) willReturn:vimController];
+  [given([mainWindowController window]) willReturn:window];
 
-  workspace = [[VRWorkspace alloc] init];
-  workspace.mainWindowController = mainWindowController;
+  workspace.mainWindowControllerFactory = mainWindowControllerFactory;
+
+  [workspace setUpWithVimController:vimController];
+}
+
+- (void)testSelectUrl {
+  NSURL *url = [NSURL URLWithString:@"file:///some/file"];
+  [workspace selectBufferWithUrl:url];
+
+  [verify(vimController) gotoBufferWithUrl:url];
+  [verify(window) makeKeyAndOrderFront:anything()];
 }
 
 - (void)testHasModifiedBuffer {
   [given([vimController hasModifiedBuffer]) willReturnBool:YES];
-  assertThat(@(workspace.hasModifiedBuffer), isYes);
+  (@(workspace.hasModifiedBuffer), isYes);
 
   [given([vimController hasModifiedBuffer]) willReturnBool:NO];
-  assertThat(@(workspace.hasModifiedBuffer), isNo);
+  (@(workspace.hasModifiedBuffer), isNo);
 }
 
 - (void)notTestSetUpWithVimController {
