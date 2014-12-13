0e0b045fac4bd549bddfedd25fbb7233d86ab13f
Tae Won Ha <qvacua@me.com>
2014-12-13 13:37:15 +0100

    #51 add open in tabs script

---
+++ VimR/VROpenFileInNewWindows.h
@@ -0,0 +1,18 @@
+/**
+* Tae Won Ha — @hataewon
+*
+* http://taewon.de
+* http://qvacua.com
+*
+* See LICENSE
+*/
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface VROpenFileInNewWindows : NSObject
+
+#pragma mark NSScriptCommand
+- (id)performDefaultImplementation;
+
+@end
