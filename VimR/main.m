d67decb8abfba13dee3c7ddcebf2f690a1f4e86f
Tae Won Ha <qvacua@me.com>
2014-12-20 20:56:58 +0100

    MISC reformat

--- VimR/main.m
+++ VimR/main.m
@@ -9,7 +9,8 @@
 
 
 int main(int argc, const char *argv[]) {
-  /** Usually we would call here the following:
+  /**
+  *  Usually we would call here the following:
   * [[TBContext sharedContext] initContext];
   * However, we do that in the -init of the app delegate because we use a subclass of NSApplication. Otherwise, AppKit
   * tries to instantiate two NSApplication's.
