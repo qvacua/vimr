136c2dcbac6637079266ec84c3cce3d923e01e09
Tae Won Ha <h@taewon.de>
2015-02-07 09:33:09 +0100

    #165 re-add deleted tests

--- VimRTests/VROpenQuicklyIgnorePatternTest.m
+++ VimRTests/VROpenQuicklyIgnorePatternTest.m
@@ -32,4 +32,34 @@
   assertThat(@([pattern matchesPath:@"/a/b/c/.hg/d"]), isNo);
 }
 
+- (void)testMatchSuffix {
+  pattern = [[VROpenQuicklyIgnorePattern alloc] initWithPattern:@"*.png"];
+
+  assertThat(@([pattern matchesPath:@"/a/b/c/d.png"]), isYes);
+  assertThat(@([pattern matchesPath:@"a.png"]), isYes);
+
+  assertThat(@([pattern matchesPath:@"/a/b/c/d.pnge"]), isNo);
+  assertThat(@([pattern matchesPath:@"/a/b/c/d.png/e"]), isNo);
+}
+
+- (void)testMatchPrefix {
+  pattern = [[VROpenQuicklyIgnorePattern alloc] initWithPattern:@"vr*"];
+
+  assertThat(@([pattern matchesPath:@"/a/b/c/vr.png"]), isYes);
+  assertThat(@([pattern matchesPath:@"vr.png"]), isYes);
+
+  assertThat(@([pattern matchesPath:@"/a/b/c/wvr.png"]), isNo);
+  assertThat(@([pattern matchesPath:@"/a/b/c/wvr.png/e"]), isNo);
+}
+
+- (void)testMatchExact {
+  pattern = [[VROpenQuicklyIgnorePattern alloc] initWithPattern:@"some"];
+
+  assertThat(@([pattern matchesPath:@"/a/b/c/some"]), isYes);
+  assertThat(@([pattern matchesPath:@"some"]), isYes);
+
+  assertThat(@([pattern matchesPath:@"/a/b/c/some1"]), isNo);
+  assertThat(@([pattern matchesPath:@"/a/b/c/1some"]), isNo);
+}
+
 @end
