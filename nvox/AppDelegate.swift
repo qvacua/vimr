10eb204539c975cfd9c7bec5bb00f3c2f697bf44
Tae Won Ha <h@taewon.de>
2016-06-03 22:13:59 +0200

    Initial Commit

---
+++ nvox/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  nvox
+//
+//  Created by Tae Won Ha on 03/06/16.
+//  Copyright © 2016 Tae Won Ha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
