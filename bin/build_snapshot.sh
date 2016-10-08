1a471bd554127b08d3405ddbe33febe641be3aa3
Tae Won Ha <h@taewon.de>
2016-10-08 17:47:15 +0200

    GH-288 Copy the full runtime folder into app package

--- bin/build_snapshot.sh
+++ bin/build_snapshot.sh
@@ -22,12 +22,26 @@ rm -rf build
 
 git submodule update --init
 
-# delete previously built libnvim
+# Build NeoVim
+# 0. Delete previously built things
+# 1. Build normally to get the full runtime folder and copy it to the neovim's project root
+# 2. Delete the build folder to re-configure
+# 3. Build libnvim
 pushd neovim
+
 ln -f -s ../local.mk .
-rm -rf build
+
 make distclean
-make CMAKE_BUILD_TYPE=Release libnvim
+
+make CMAKE_FLAGS="-DCUSTOM_UI=0"
+make CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=/tmp/nvim" install
+cp -r /tmp/nvim/share/nvim/runtime .
+
+make clean
+rm -rf build
+
+make libnvim
+
 popd
 
 carthage update --platform osx
