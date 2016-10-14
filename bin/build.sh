da5b0388fbc1ccab75f6113fd8ee473f56f6606f
Tae Won Ha <h@taewon.de>
2016-10-14 23:19:48 +0200

    GH-220 Fix script

--- bin/build.sh
+++ bin/build.sh
@@ -20,13 +20,13 @@ export PATH=/usr/local/bin:$PATH
 BUNDLE_VERSION=$(agvtool what-version | sed '2q;d' | sed -E 's/ +(.+)/\1/')
 MARKETING_VERSION=$(agvtool what-marketing-version | tail -n 1 | sed -E 's/.*of "(.*)" in.*/\1/')
 COMPOUND_VERSION="v${MARKETING_VERSION}-${BUNDLE_VERSION}"
-if [ ${IS_SNAPSHOT} = true ] ; then
+if [ "${IS_SNAPSHOT}" = true ] ; then
     COMPOUND_VERSION="v${MARKETING_VERSION}-${BUNDLE_VERSION}"
 fi
 TAG=${COMPOUND_VERSION}
 VIMR_FILE_NAME="VimR-${COMPOUND_VERSION}.tar.bz2"
 
-./bin/commit_and_push_tags.sh "${BRANCH}" "${TAG}"
+./bin/commit_and_push_tags.sh "${BRANCH}" "${TAG}" ${IS_SNAPSHOT}
 
 ./bin/create_github_release.sh "${TAG}" "${VIMR_FILE_NAME}" "${RELEASE_NOTES}"
 
