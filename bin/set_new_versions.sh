d776f513618f63a1d193429b84ff4b58bb4d263f
Tae Won Ha <h@taewon.de>
2016-10-14 23:55:38 +0200

    GH-220 Improve script

--- bin/set_new_versions.sh
+++ bin/set_new_versions.sh
@@ -10,8 +10,9 @@ agvtool bump -all
 
 # ## marketing version
 if [ "${IS_SNAPSHOT}" = true ] ; then
-    MARKETING_VERSION=$(agvtool what-marketing-version | tail -n 1 | sed -E 's/.*of "(.*)" in.*/\1/' | sed -E "s/(.*)-SNAPSHOT-.*/\1-SNAPSHOT-$(date +%Y%m%d.%H%M)/")
+    MARKETING_VERSION=$(agvtool what-marketing-version | tail -n 1 | sed -E 's/.*of "(.*)" in.*/\1/' | sed -E "s/^([0-9]+)\.([0-9]+)\.([0-9]+).*/\1.\2.\3-SNAPSHOT-$(date +%Y%m%d.%H%M)/")
 fi
+
 agvtool new-marketing-version ${MARKETING_VERSION}
 
 echo "### Set versions of VimR"
