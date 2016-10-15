9421ae1f4dcecbb0aa0206101d60334a9417d6bc
Tae Won Ha <h@taewon.de>
2016-10-15 09:47:11 +0200

    GH-220 Improve script

--- bin/commit_and_push_appcast.sh
+++ bin/commit_and_push_appcast.sh
@@ -2,9 +2,16 @@
 
 BRANCH=$1
 COMPOUND_VERSION=$2
+IS_SNAPSHOT=$3
 
-cp build/Release/appcast* .
+if [ "${IS_SNAPSHOT}" = true ] ; then
+    cp ./build/Release/appcast_snapshot.xml .
+else
+    cp ./build/Release/appcast.xml .
+    cp ./build/Release/appcast.xml ./appcast_snapshot.xml
+fi
 
 echo "### Commiting and pushing appcast"
+
 git commit -am "Bump appcast to ${COMPOUND_VERSION}"
 git push origin HEAD:"${BRANCH}"
