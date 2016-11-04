3b95b3d4d87804c4998f0b7f3cf8ffcf0c1dbe7f
Tae Won Ha <h@taewon.de>
2016-11-04 21:41:36 +0100

    Adapt bulid script

--- bin/prepare_repositories.sh
+++ bin/prepare_repositories.sh
@@ -7,6 +7,10 @@ echo "### Preparing repositories"
 # delete all (local) tags
 git tag | xargs git tag -d
 
+pushd neovim
+git tag | xargs git tag -d
+popd
+
 # delete all (local) branches
 git for-each-ref --format="%(refname:strip=2)" refs/heads/ | xargs git branch -D
 git checkout -b for_build
