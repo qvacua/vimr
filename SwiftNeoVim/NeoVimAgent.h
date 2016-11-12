4933feee608358011d2d3e5d643f38eb1bddd8f7
Tae Won Ha <h@taewon.de>
2016-11-12 11:31:22 +0100

    GH-299 Remove unused function decl

--- SwiftNeoVim/NeoVimAgent.h
+++ SwiftNeoVim/NeoVimAgent.h
@@ -27,7 +27,6 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)vimCommand:(NSString *)string;
 - (NSString *)vimCommandOutput:(NSString *)string;
 
-- (void)vimInputSync:(NSString *)string;
 - (void)vimInput:(NSString *)string;
 - (void)vimInputMarkedText:(NSString *_Nonnull)markedText;
 - (void)deleteCharacters:(NSInteger)count;
