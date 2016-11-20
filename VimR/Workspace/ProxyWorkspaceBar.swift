8a937fd75df7b694b9a3157bff583a2f3575f3e7
Tae Won Ha <h@taewon.de>
2016-11-20 10:40:57 +0100

    GH-296 Add a comment and reformat

--- VimR/Workspace/ProxyWorkspaceBar.swift
+++ VimR/Workspace/ProxyWorkspaceBar.swift
@@ -6,19 +6,22 @@
 import Cocoa
 import PureLayout
 
+/**
+ This class is used to display the placeholder bar when a tool is drag & dropped to a location with no existing tools.
+ */
 class ProxyWorkspaceBar: NSView {
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 
   override init(frame: NSRect) {
     super.init(frame: frame)
 
     // Because other views also want layer, this view also must want layer. Otherwise the z-index ordering is not set
-    // correctly: views w wantsLayer = false are behind views with wantsLayer = true even when added later.
+    // correctly: views w/ wantsLayer = false are behind views w/ wantsLayer = true even when added later.
     self.wantsLayer = true
     self.layer?.backgroundColor = NSColor.windowBackgroundColor.cgColor
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
   }
 
   override func draw(_ dirtyRect: NSRect) {
