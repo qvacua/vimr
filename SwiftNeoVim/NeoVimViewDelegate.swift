1a3c348528efaa9cdee470fb5b0248c37c801f2e
Tae Won Ha <h@taewon.de>
2016-11-20 21:55:51 +0100

    GH-297 Reformat

--- SwiftNeoVim/NeoVimViewDelegate.swift
+++ SwiftNeoVim/NeoVimViewDelegate.swift
@@ -7,9 +7,9 @@ import Cocoa
 
 // See http://stackoverflow.com/a/24104371 for class
 public protocol NeoVimViewDelegate: class {
-  
+
+  func neoVimStopped()
   func set(title: String)
   func set(dirtyStatus: Bool)
   func cwdChanged()
-  func neoVimStopped()
 }
