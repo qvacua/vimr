13b5d11ec8b23028e8d3ce15873a03c5b36e0813
Tae Won Ha <h@taewon.de>
2016-11-27 16:41:52 +0100

    GH-293 Add stub buttons

--- VimR-Workspace-Demo/AppDelegate.swift
+++ VimR-Workspace-Demo/AppDelegate.swift
@@ -23,34 +23,65 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     let contentView = self.window.contentView!
-    let workspace = Workspace(mainView: DummyView(NSColor.yellow))
+    let workspace = Workspace(mainView: DummyView(NSColor.white))
     self.workspace = workspace
 
     contentView.addSubview(workspace)
     workspace.autoPinEdgesToSuperviewEdges()
 
-    workspace.append(tool: WorkspaceTool(title: "Top-1", view: DummyView(NSColor.white)), location: .top)
-    workspace.append(tool: WorkspaceTool(title: "Right-1", view: DummyView(NSColor.white)), location: .right)
-    workspace.append(tool: WorkspaceTool(title: "Right-2", view: DummyView(NSColor.green)), location: .right)
-    workspace.append(tool: WorkspaceTool(title: "Left-1", view: DummyView(NSColor.white)), location: .left)
-    workspace.append(tool: WorkspaceTool(title: "Left-2", view: DummyView(NSColor.green)), location: .left)
-    workspace.append(tool: WorkspaceTool(title: "Left-3", view: DummyView(NSColor.magenta)), location: .left)
-    workspace.append(tool: WorkspaceTool(title: "Bottom-1", view: DummyView(NSColor.white)), location: .bottom)
-    workspace.append(tool: WorkspaceTool(title: "Bottom-2", view: DummyView(NSColor.green)), location: .bottom)
+    workspace.append(tool: WorkspaceTool(title: "Top-1", view: DummyToolView(NSColor.yellow)), location: .top)
+    workspace.append(tool: WorkspaceTool(title: "Right-1", view: DummyToolView(NSColor.magenta)), location: .right)
+    workspace.append(tool: WorkspaceTool(title: "Right-2", view: DummyToolView(NSColor.black)), location: .right)
+    let tool = WorkspaceTool(title: "Left-1", view: DummyToolView(NSColor.green))
+    workspace.append(tool: tool, location: .left)
+    workspace.append(tool: WorkspaceTool(title: "Left-2", view: DummyToolView(NSColor.red)), location: .left)
+    workspace.append(tool: WorkspaceTool(title: "Left-3", view: DummyToolView(NSColor.gray)), location: .left)
+    workspace.append(tool: WorkspaceTool(title: "Bottom-1", view: DummyToolView(NSColor.cyan)), location: .bottom)
+    workspace.append(tool: WorkspaceTool(title: "Bottom-2", view: DummyToolView(NSColor.blue)), location: .bottom)
+
+    let left = workspace.bars[.left]!
+    left.toggle(tool)
+  }
+}
+
+class DummyToolView: NSView {
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  init(_ color: NSColor) {
+    super.init(frame: .zero)
+    self.configureForAutoLayout()
+    self.wantsLayer = true
+
+    let innerToolbar = InnerToolBar(forAutoLayout:())
+    let dummyView = DummyView(color)
+
+    self.addSubview(innerToolbar)
+    self.addSubview(dummyView)
+
+    innerToolbar.autoPinEdge(toSuperviewEdge: .top)
+    innerToolbar.autoPinEdge(toSuperviewEdge: .right)
+    innerToolbar.autoPinEdge(toSuperviewEdge: .left)
+
+    dummyView.autoPinEdge(.top, to: .bottom, of: innerToolbar)
+    dummyView.autoPinEdge(toSuperviewEdge: .right)
+    dummyView.autoPinEdge(toSuperviewEdge: .left)
   }
 }
 
 class DummyView: NSView {
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 
   init(_ color: NSColor) {
     super.init(frame: .zero)
     self.configureForAutoLayout()
     self.wantsLayer = true
     self.layer?.backgroundColor = color.cgColor
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
   }
 
   override func mouseDown(with event: NSEvent) {
--- VimR/Workspace/InnterToolBar.swift
+++ VimR/Workspace/InnterToolBar.swift
@@ -13,12 +13,22 @@ import PureLayout
  */
 class InnerToolBar: NSView {
 
+  static fileprivate let separatorColor = NSColor.controlShadowColor
+  static fileprivate let separatorThickness = CGFloat(1)
+
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
 
+  fileprivate let closeButton = NSButton(forAutoLayout:())
+  fileprivate let cogButton = NSButton(forAutoLayout:())
+
   override var intrinsicContentSize: CGSize {
-    return CGSize(width: NSViewNoIntrinsicMetrix, height: 24)
+    if #available(macOS 10.11, *) {
+      return CGSize(width: NSViewNoIntrinsicMetric, height: 24 + InnerToolBar.separatorThickness)
+    } else {
+      return CGSize(width: -1, height: 24)
+    }
   }
 
   override init(frame: NSRect) {
@@ -28,6 +38,49 @@ class InnerToolBar: NSView {
     // correctly: views w/ wantsLayer = false are behind views w/ wantsLayer = true even when added later.
     self.wantsLayer = true
     self.layer?.backgroundColor = NSColor.yellow.cgColor
+
+    self.addViews()
+  }
+
+  override func draw(_ dirtyRect: NSRect) {
+    InnerToolBar.separatorColor.set()
+
+    let separatorRect = self.bottomSeparatorRect()
+    if dirtyRect.intersects(separatorRect) {
+      NSRectFill(separatorRect)
+    }
+  }
+
+  fileprivate func addViews() {
+    let close = self.closeButton
+    let cog = self.cogButton
+
+    close.imagePosition = .imageOnly
+    close.isBordered = false
+
+    cog.imagePosition = .imageOnly
+    cog.isBordered = false
+
+    self.addSubview(close)
+    self.addSubview(cog)
+
+    close.autoPinEdge(toSuperviewEdge: .top, withInset: 2)
+    close.autoPinEdge(toSuperviewEdge: .right, withInset: 2)
+    close.autoSetDimension(.width, toSize: 18)
+    close.autoSetDimension(.height, toSize: 18)
+
+    cog.autoPinEdge(toSuperviewEdge: .top, withInset: 2)
+    cog.autoPinEdge(.right, to: .left, of: close, withOffset: -2)
+    cog.autoSetDimension(.width, toSize: 18)
+    cog.autoSetDimension(.height, toSize: 18)
+
+    close.image = NSImage(named: NSImageNameStopProgressTemplate)
+    cog.image = NSImage(named: NSImageNameActionTemplate)
+  }
+
+  fileprivate func bottomSeparatorRect() -> CGRect {
+    let bounds = self.bounds
+    return CGRect(x: bounds.minX, y: bounds.minY, width: bounds.width, height: InnerToolBar.separatorThickness)
   }
 }
 
