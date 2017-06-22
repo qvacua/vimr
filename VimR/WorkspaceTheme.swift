3cc03cf4036a621de73d0aa1c3d6c78f59782125
Tae Won Ha <h@taewon.de>
2017-06-22 23:05:21 +0200

    GH-436 Add a theme struct

---
+++ VimR/WorkspaceTheme.swift
@@ -0,0 +1,20 @@
+/**
+ * Tae Won Ha - http://taewon.de - @hataewon
+ * See LICENSE
+ */
+
+import Cocoa
+
+struct WorkspaceTheme {
+
+  var foreground = NSColor.black
+  var background = NSColor.white
+
+  var separator = NSColor.controlShadowColor
+
+  var barBackground = NSColor.windowBackgroundColor
+  var barButtonHighlight = NSColor.controlShadowColor
+
+  var toolbarForeground = NSColor.darkGray
+  var toolbarBackground = NSColor(red: 0.899, green: 0.934, blue: 0.997, alpha: 1)
+}
