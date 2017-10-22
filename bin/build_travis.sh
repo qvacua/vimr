0a78357993ec0c0a6a628ba60b0ad9b4961cd08a
Tae Won Ha <h@taewon.de>
2017-10-22 19:38:36 +0200

    GH-544 Separate build and testing

--- bin/build_travis.sh
+++ bin/build_travis.sh
@@ -18,6 +18,11 @@ popd
 echo "### Updating carthage"
 carthage update --platform osx --cache-builds
 
+# to avoid Xcode time out, cf https://stackoverflow.com/questions/37922146/xctests-failing-on-physical-device-canceling-tests-due-to-timeout/40790171#40790171
+echo "### Building"
+xcodebuild build -scheme SwiftNeoVim
+xcodebuild build -scheme VimR
+
 echo "### Executing tests"
 xcodebuild test -scheme SwiftNeoVim
 xcodebuild test -scheme VimR
