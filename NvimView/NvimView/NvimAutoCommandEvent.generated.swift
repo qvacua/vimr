f47174b69e7f3b347e21abd655997ac780743663
Tae Won Ha <h@taewon.de>
2018-01-27 12:40:49 +0100

    Generate sources

--- NvimView/NvimView/NvimAutoCommandEvent.generated.swift
+++ NvimView/NvimView/NvimAutoCommandEvent.generated.swift
@@ -1,5 +1,5 @@
-// Auto generated for nvim v0.2.2.
-// See bin/generate_source.py
+// Auto generated for nvim v0.2.3-dev.
+// See bin/generate_autocmds.py
 
 enum NvimAutoCommandEvent: Int {
   case bufadd = 0
