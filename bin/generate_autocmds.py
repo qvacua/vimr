ea1b0847c0598eea5dd277fa54524668087444f1
Tae Won Ha <h@taewon.de>
2018-01-27 12:29:49 +0100

    Refactor source generation

--- bin/generate_autocmds.py
+++ bin/generate_autocmds.py
@@ -11,14 +11,14 @@ def convert(line):
     return result.group(1), result.group(2)
 
 
-def swift_auto_cmds():
+def swift_auto_cmds(version):
     with io.open('./neovim/build/include/auevents_enum.generated.h', 'r') as auto_cmds_file:
         raw_auto_cmds = [line.strip() for line in auto_cmds_file.readlines() if re.match(r'^EVENT_', line.strip())]
 
     auto_cmds = [convert(line) for line in raw_auto_cmds]
     auto_cmds_template = Template(
-'''// Auto generated for nvim version 0.2.2.
-// See bin/generate_source.py
+'''// Auto generated for nvim ${version}.
+// See bin/generate_autocmds.py
 
 enum NvimAutoCommandEvent: Int {
 ${event_cases}
@@ -30,11 +30,13 @@ ${event_cases}
         event_cases='\n'.join(
             ['  case {} = {}'.format(event[0].lower(), event[1]) for event in auto_cmds]
         ),
+        version=version
     )
 
 
 if __name__ == '__main__':
     result_file_path = './NvimView/NvimAutoCommandEvent.generated.swift'
+    version = os.environ['VERSION']
 
     if 'CONFIGURATION' in os.environ and os.environ['CONFIGURATION'] == 'Debug':
         if os.path.isfile(result_file_path):
@@ -42,4 +44,4 @@ if __name__ == '__main__':
             exit(0)
 
     with io.open(result_file_path, 'w') as auto_cmds_header_file:
-        auto_cmds_header_file.write(swift_auto_cmds())
+        auto_cmds_header_file.write(swift_auto_cmds(version))
---
+++ bin/generate_sources.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+current_neovim_branch=$1
+
+git submodule update
+
+pushd NvimView/neovim
+git fetch
+make distclean
+mv local.mk local.mk.bak || true
+git checkout "origin/last-merge-${current_neovim_branch}"
+make
+popd
+
+nvim_bin_path="./NvimView/neovim/build/bin/nvim"
+nvim_version=$(${nvim_bin_path} --version | head -1 | cut -d' ' -f2)
+
+pushd NvimMsgPack
+NVIM_PATH="../${nvim_bin_path}" ../bin/generate_api_methods.py
+popd
+
+pushd NvimView
+VERSION=${nvim_version} generate_autocmds.py
+
+pushd neovim
+rm -rf build
+make clean
+git reset --hard HEAD
+git checkout ${current_neovim_branch}
+mv local.mk.bak local.mk || true
+popd
+
+popd
+
+echo "############## Successfully generated sources."
