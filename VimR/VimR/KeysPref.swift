423406adb599c70f547914cec7d19ee2a66f8ba0
Tae Won Ha <h@taewon.de>
2018-04-05 21:15:54 +0200

    GH-543 Add Keys preferences pane

---
+++ VimR/VimR/KeysPref.swift
@@ -0,0 +1,112 @@
+/**
+ * Tae Won Ha - http://taewon.de - @hataewon
+ * See LICENSE
+ */
+
+import Cocoa
+import PureLayout
+import RxSwift
+
+class KeysPref: PrefPane, UiComponent, NSTextFieldDelegate {
+
+  typealias StateType = AppState
+
+  enum Action {
+
+    case isLeftOptionMeta(Bool)
+    case isRightOptionMeta(Bool)
+  }
+
+  override var displayName: String {
+    return "Keys"
+  }
+
+  override var pinToContainer: Bool {
+    return true
+  }
+
+  required init(source: Observable<StateType>, emitter: ActionEmitter, state: StateType) {
+    self.emit = emitter.typedEmit()
+
+    self.isLeftOptionMeta = state.mainWindowTemplate.isLeftOptionMeta
+    self.isRightOptionMeta = state.mainWindowTemplate.isRightOptionMeta
+
+    super.init(frame: .zero)
+
+    self.addViews()
+    self.updateViews()
+
+    source
+      .observeOn(MainScheduler.instance)
+      .subscribe(onNext: { state in
+        if self.isLeftOptionMeta != state.mainWindowTemplate.isLeftOptionMeta
+        || self.isRightOptionMeta != state.mainWindowTemplate.isRightOptionMeta
+        {
+          self.isLeftOptionMeta = state.mainWindowTemplate.isLeftOptionMeta
+          self.isRightOptionMeta = state.mainWindowTemplate.isRightOptionMeta
+
+          self.updateViews()
+        }
+      })
+      .disposed(by: self.disposeBag)
+  }
+
+  private let emit: (Action) -> Void
+  private let disposeBag = DisposeBag()
+
+  private var isLeftOptionMeta: Bool
+  private var isRightOptionMeta: Bool
+
+  private let isLeftOptionMetaCheckbox = NSButton(forAutoLayout: ())
+  private let isRightOptionMetaCheckbox = NSButton(forAutoLayout: ())
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func updateViews() {
+    self.isLeftOptionMetaCheckbox.boolState = self.isLeftOptionMeta
+    self.isRightOptionMetaCheckbox.boolState = self.isRightOptionMeta
+  }
+
+  private func addViews() {
+    let paneTitle = self.paneTitleTextField(title: "Keys")
+
+    let isLeftOptionMeta = self.isLeftOptionMetaCheckbox
+    self.configureCheckbox(button: isLeftOptionMeta,
+                           title: "Use Left Option as Meta",
+                           action: #selector(KeysPref.isLeftOptionMetaAction(_:)))
+
+    let isRightOptionMeta = self.isRightOptionMetaCheckbox
+    self.configureCheckbox(button: isRightOptionMeta,
+                           title: "Use Right Option as Meta",
+                           action: #selector(KeysPref.isRightOptionMetaAction(_:)))
+
+    self.addSubview(paneTitle)
+
+    self.addSubview(isLeftOptionMeta)
+    self.addSubview(isRightOptionMeta)
+
+    paneTitle.autoPinEdge(toSuperviewEdge: .top, withInset: 18)
+    paneTitle.autoPinEdge(toSuperviewEdge: .left, withInset: 18)
+    paneTitle.autoPinEdge(toSuperviewEdge: .right, withInset: 18, relation: .greaterThanOrEqual)
+
+    isLeftOptionMeta.autoPinEdge(.top, to: .bottom, of: paneTitle, withOffset: 18)
+    isLeftOptionMeta.autoPinEdge(toSuperviewEdge: .left, withInset: 18)
+
+    isRightOptionMeta.autoPinEdge(.top, to: .bottom, of: isLeftOptionMeta, withOffset: 5)
+    isRightOptionMeta.autoPinEdge(toSuperviewEdge: .left, withInset: 18)
+  }
+}
+
+// MARK: - Actions
+extension KeysPref {
+
+  @objc func isLeftOptionMetaAction(_ sender: NSButton) {
+    self.emit(.isLeftOptionMeta(sender.boolState))
+  }
+
+  @objc func isRightOptionMetaAction(_ sender: NSButton) {
+    self.emit(.isRightOptionMeta(sender.boolState))
+  }
+}
