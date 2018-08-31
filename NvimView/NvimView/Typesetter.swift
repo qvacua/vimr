f0dfd94d9a36c85450fbefdbc6428c27cf9391dd
Tae Won Ha <h@taewon.de>
2018-08-31 16:37:29 +0200

    GH-666 Remove old (slow) run with ligatures method

--- NvimView/NvimView/Typesetter.swift
+++ NvimView/NvimView/Typesetter.swift
@@ -5,34 +5,6 @@
 
 import Cocoa
 
-struct PositionedUtf16Cell {
-
-  var utf16: [Unicode.UTF16.CodeUnit]
-  var column: Int
-}
-
-struct CellIndexedUtf16Char {
-
-  var codeUnit: Unicode.UTF16.CodeUnit
-  var index: Int
-}
-
-struct Utf16IndexedGlyph {
-
-  var glyph: CGGlyph
-
-  var index: CFIndex
-
-  var position: CGPoint
-  var advance: CGSize
-}
-
-struct CellGlyphUnion {
-
-  var positionedUtf16Cells: [PositionedUtf16Cell]
-  var utf16IndexedGlyphs: [Utf16IndexedGlyph]
-}
-
 class Typesetter {
 
   final func fontGlyphRunsWithLigatures(
@@ -110,7 +82,7 @@ class Typesetter {
     var cellIndex = 0
     var i = 0
     repeat {
-      defer { cellIndex += 1}
+      defer { cellIndex += 1 }
 
       if nvimUtf16Cells[cellIndex].isEmpty {
         continue
@@ -134,83 +106,6 @@ class Typesetter {
     }
 
     return utf16Chars
-  }
-
-  final func fontGlyphRunsWithLigatures_old(
-    nvimUtf16Cells: [[Unicode.UTF16.CodeUnit]],
-    startColumn: Int,
-    offset: CGPoint,
-    foreground: Int,
-    font: NSFont,
-    cellWidth: CGFloat
-  ) -> [FontGlyphRun] {
-
-    let utf16Chars = nvimUtf16Cells.flatMap { $0 }
-    let ctRuns = self.ctRuns(
-      from: utf16Chars, font: font, foreground: foreground
-    )
-    let utf16IndexedGlyphs = self.utf16IndexedGlyphs(from: ctRuns)
-
-    let cellIndexedUtf16Chars = nvimUtf16Cells
-      .filter { !$0.isEmpty }
-      .enumerated()
-      .map { element in
-        element.1.map { utf16 in
-          CellIndexedUtf16Char(codeUnit: utf16, index: element.0)
-        }
-      }
-      .flatMap { $0 }
-
-    let positionedCells = nvimUtf16Cells
-      .enumerated()
-      .compactMap { element -> PositionedUtf16Cell? in
-        if element.1.isEmpty { return nil }
-
-        return PositionedUtf16Cell(
-          utf16: element.1, column: startColumn + element.0
-        )
-      }
-
-    let groupedCellsAndGlyphs = self.groupUtf16CellsAndGlyphs(
-      positionedCells: positionedCells,
-      cellIndexedUtf16Chars: cellIndexedUtf16Chars,
-      utf16IndexedGlyphs: utf16IndexedGlyphs
-    )
-
-    let stringRanges = ctRuns.map { run -> CountableRange<Int> in
-      let range = CTRunGetStringRange(run)
-      return range.location..<(range.location + range.length)
-    }
-
-    let fonts = ctRuns.map { run -> NSFont in
-      guard
-        let attrs = CTRunGetAttributes(run) as? [NSAttributedStringKey: Any],
-        let font = attrs[NSAttributedStringKey.font] as? NSFont
-        else {
-        // FIXME: GH-666: Return the default font
-        preconditionFailure("Could not get font from CTRun!")
-      }
-
-      return font
-    }
-
-    let fontRanges = self.groupByStringRanges(
-      stringRanges: stringRanges, groupedCellsAndGlyphs: groupedCellsAndGlyphs
-    )
-
-    let cellsAndGlyphsGroupedByFont = fontRanges.map { range in
-      Array(groupedCellsAndGlyphs[range])
-    }
-
-    let fontGlyphRuns = self.fontGlyphRuns(
-      fonts: fonts,
-      cellsAndGlyphsGroupedByFont: cellsAndGlyphsGroupedByFont,
-      estimatedGlyphsCount: utf16Chars.count,
-      cellWidth: cellWidth,
-      offset: offset
-    )
-
-    return fontGlyphRuns
   }
 
   final func fontGlyphRunsWithoutLigatures(
@@ -294,146 +189,6 @@ class Typesetter {
     return runs.flatMap { $0 }
   }
 
-  // For testing internal
-  func groupUtf16CellsAndGlyphs(
-    positionedCells: [PositionedUtf16Cell],
-    cellIndexedUtf16Chars: [CellIndexedUtf16Char],
-    utf16IndexedGlyphs: [Utf16IndexedGlyph]
-  ) -> [CellGlyphUnion] {
-
-    let utf16IndicesOfGlyphs = utf16IndexedGlyphs.map { $0.index }.uniqued()
-    let extendedUtf16Indices
-      = utf16IndicesOfGlyphs + [cellIndexedUtf16Chars.count]
-
-    let utf16RangesOfGlyphs = (0..<utf16IndicesOfGlyphs.count).map { i in
-      extendedUtf16Indices[i]..<extendedUtf16Indices[i + 1]
-    }
-
-    let nonUniqueGroupedUtf16CellIndices = utf16RangesOfGlyphs.map { range in
-      cellIndexedUtf16Chars[range]
-        .map { $0.index }
-        .uniqued()
-    }
-
-    var groupedUtf16CellIndices = [[Int]]()
-    groupedUtf16CellIndices.reserveCapacity(
-      nonUniqueGroupedUtf16CellIndices.count
-    )
-    var lastIndex = -1
-    nonUniqueGroupedUtf16CellIndices.forEach { element in
-      guard let first = element.first, let last = element.last else {
-        preconditionFailure("There are no first and last element in " +
-                              "a grouped UTF16 cell indices " +
-                              "non-unique collection!")
-      }
-
-      guard first > lastIndex else { return }
-      groupedUtf16CellIndices.append(element)
-      lastIndex = last
-    }
-
-    let groupedUtf16CellRanges = groupedUtf16CellIndices
-      .map { ind -> CountableClosedRange<Int> in
-
-      guard let first = ind.first, let last = ind.last else {
-        preconditionFailure("There are no first and last element in " +
-                              "a grouped UTF16 cell indices!")
-      }
-
-      return first...last
-    }
-
-    let partitionedUtf16Cells = groupedUtf16CellRanges.map { range in
-      Array(positionedCells[range])
-    }
-
-    let partitionedGlyphs = utf16IndexedGlyphs
-      .map { cellIndexedUtf16Chars[$0.index].index }
-      .groupedRanges { _, cellIndexOfUtf16Index, _ in cellIndexOfUtf16Index }
-      .map { Array(utf16IndexedGlyphs[$0]) }
-
-    return zip(partitionedUtf16Cells, partitionedGlyphs)
-      .map { zip in
-        CellGlyphUnion(
-          positionedUtf16Cells: zip.0,
-          utf16IndexedGlyphs: zip.1
-        )
-      }
-  }
-
-  // For testing internal
-  func groupByStringRanges(
-    stringRanges: [CountableRange<Int>],
-    groupedCellsAndGlyphs: [CellGlyphUnion]
-  ) -> [CountableClosedRange<Int>] {
-    var lastLength = 0
-    var lastIndex = 0
-
-    var result = Array<CountableClosedRange<Int>>()
-    result.reserveCapacity(stringRanges.count)
-
-    for range in stringRanges {
-      for i in (lastIndex..<groupedCellsAndGlyphs.count) {
-        lastLength += groupedCellsAndGlyphs[i]
-          .positionedUtf16Cells
-          .reduce(0) { result, element in
-          result + element.utf16.count
-        }
-        if lastLength == range.upperBound {
-          result.append(lastIndex...i)
-          lastIndex = i + 1
-          break
-        }
-      }
-    }
-
-    return result
-  }
-
-  private func fontGlyphRuns(
-    fonts: [NSFont],
-    cellsAndGlyphsGroupedByFont: [[CellGlyphUnion]],
-    estimatedGlyphsCount: Int,
-    cellWidth: CGFloat,
-    offset: CGPoint
-  ) -> [FontGlyphRun] {
-    let zipped = zip(fonts, cellsAndGlyphsGroupedByFont)
-    let fontGlyphRuns = zipped.map { zip -> FontGlyphRun in
-      let font = zip.0
-      let cellsAndGlyphs = zip.1
-
-      var glyphs = Array<CGGlyph>()
-      glyphs.reserveCapacity(estimatedGlyphsCount)
-      var positions = Array<CGPoint>()
-      positions.reserveCapacity(estimatedGlyphsCount)
-
-      for element in cellsAndGlyphs {
-        let cells = element.positionedUtf16Cells
-        let indexedGlyphs = element.utf16IndexedGlyphs
-
-        let startColumnPosition
-          = offset.x + CGFloat(cells[0].column) * cellWidth
-        let deltaX = startColumnPosition - indexedGlyphs[0].position.x
-
-        glyphs.append(contentsOf: indexedGlyphs.map { $0.glyph })
-        positions.append(contentsOf: indexedGlyphs.map { indexedGlyph in
-          CGPoint(x: indexedGlyph.position.x + deltaX, y: offset.y)
-        })
-      }
-
-      // FIXME: GH-666: Proper error handling
-      if glyphs.count != positions.count {
-        print("Counts of glyphs and positions are not the same")
-      }
-
-      return FontGlyphRun(
-        font: font, glyphs: glyphs, positions: positions
-      )
-    }
-
-    return fontGlyphRuns
-  }
-
   private func ctRuns(
     from utf16Chars: [Unicode.UTF16.CodeUnit],
     font: NSFont,
@@ -451,39 +206,6 @@ class Typesetter {
 
     guard let ctRuns = CTLineGetGlyphRuns(ctLine) as? [CTRun] else { return [] }
     return ctRuns
-  }
-
-  private func utf16IndexedGlyphs(
-    from ctRuns: [CTRun]
-  ) -> [Utf16IndexedGlyph] {
-    let utf16IndexedGlyphs = ctRuns
-      .map { run -> [Utf16IndexedGlyph] in
-        let glyphCount = CTRunGetGlyphCount(run)
-
-        var indices = Array(repeating: CFIndex(), count: glyphCount)
-        CTRunGetStringIndices(run, .zero, &indices)
-
-        var glyphs = Array(repeating: CGGlyph(), count: glyphCount)
-        CTRunGetGlyphs(run, .zero, &glyphs)
-
-        var positions = Array(repeating: CGPoint.zero, count: glyphCount)
-        CTRunGetPositions(run, .zero, &positions)
-
-        var advances = Array(repeating: CGSize.zero, count: glyphCount)
-        CTRunGetAdvances(run, .zero, &advances)
-
-        return (0..<glyphCount).map { i in
-          Utf16IndexedGlyph(
-            glyph: glyphs[i],
-            index: indices[i],
-            position: positions[i],
-            advance: advances[i]
-          )
-        }
-      }
-      .flatMap { $0 }
-
-    return utf16IndexedGlyphs
   }
 
   private struct NvimUtf16CellsRun {
