2741ee559a3589e4613b8c70236318a22e7c49cf
Tae Won Ha <h@taewon.de>
2019-03-03 22:30:40 +0100

    Restructure slightly

---
+++ NvimView/NvimView/SharedTypes.h
@@ -0,0 +1,60 @@
+/**
+ * Tae Won Ha - http://taewon.de - @hataewon
+ * See LICENSE
+ */
+
+#import <Foundation/Foundation.h>
+
+typedef NS_OPTIONS(NSUInteger, FontTrait) {
+  FontTraitNone = 0,
+  FontTraitItalic = (1 << 0),
+  FontTraitBold = (1 << 1),
+  FontTraitUnderline = (1 << 2),
+  FontTraitUndercurl = (1 << 3)
+};
+
+typedef NS_ENUM(NSInteger, RenderDataType) {
+  RenderDataTypeRawLine,
+  RenderDataTypeGoto,
+  RenderDataTypeScroll,
+};
+
+typedef NS_ENUM(NSInteger, NvimServerMsgId) {
+  NvimServerMsgIdServerReady = 0,
+  NvimServerMsgIdNvimReady,
+  NvimServerMsgIdResize,
+  NvimServerMsgIdClear,
+  NvimServerMsgIdSetMenu,
+  NvimServerMsgIdBusyStart,
+  NvimServerMsgIdBusyStop,
+  NvimServerMsgIdModeChange,
+  NvimServerMsgIdBell,
+  NvimServerMsgIdVisualBell,
+  NvimServerMsgIdFlush,
+  NvimServerMsgIdHighlightAttrs,
+  NvimServerMsgIdSetTitle,
+  NvimServerMsgIdStop,
+  NvimServerMsgIdOptionSet,
+  
+  NvimServerMsgIdDirtyStatusChanged,
+  NvimServerMsgIdCwdChanged,
+  NvimServerMsgIdColorSchemeChanged,
+  NvimServerMsgIdDefaultColorsChanged,
+  NvimServerMsgIdAutoCommandEvent,
+  NvimServerMsgIdRpcEventSubscribed,
+
+  NvimServerMsgIdDebug1,
+};
+
+typedef NS_ENUM(NSInteger, NvimBridgeMsgId) {
+  NvimBridgeMsgIdAgentReady = 0,
+  NvimBridgeMsgIdReadyForRpcEvents,
+  NvimBridgeMsgIdInput,
+  NvimBridgeMsgIdDeleteInput,
+  NvimBridgeMsgIdResize,
+  NvimBridgeMsgIdScroll,
+  
+  NvimBridgeMsgIdFocusGained,
+  
+  NvimBridgeMsgIdDebug1,
+};
