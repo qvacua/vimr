d126786bbc133d4da43c1f24ba443e24fd805fdd
Tae Won Ha <h@taewon.de>
2019-03-10 09:32:53 +0100

    Add a new target

---
+++ NvimView/DrawerPerf/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  DrawerPerf
+//
+//  Created by Tae Won Ha on 10.03.19.
+//  Copyright © 2019 Tae Won Ha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+}
