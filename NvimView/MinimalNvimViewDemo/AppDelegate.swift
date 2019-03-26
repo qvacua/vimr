4dae0bfd65e215b84461b58e586f421736330a44
Tae Won Ha <h@taewon.de>
2019-03-26 20:04:18 +0100

    Restructure project a bit

--- NvimView/MinimalNvimViewDemo/AppDelegate.swift
+++ NvimView/MinimalNvimViewDemo/AppDelegate.swift
@@ -7,7 +7,7 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+  
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     // Insert code here to initialize your application
   }
