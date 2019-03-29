8b6c7170fe97fdf941ed4ec33db88dfbfaf8e7f0
Tae Won Ha <h@taewon.de>
2019-03-29 13:54:58 +0100

    Draw parallel in the min. demo

--- NvimView/MinimalNvimViewDemo/Document.swift
+++ NvimView/MinimalNvimViewDemo/Document.swift
@@ -19,6 +19,7 @@ class Document: NSDocument, NSWindowDelegate {
     self.nvimView.font = NSFont(name: "Fira Code", size: 13)
                          ?? NSFont.userFixedPitchFont(ofSize: 13)!
     self.nvimView.usesLigatures = true
+    self.nvimView.drawsParallel = true
 
     nvimView
       .events
