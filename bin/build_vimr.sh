b111e8e6ee45e41df5a5f0d75a956c926d9fa6c2
Tae Won Ha <h@taewon.de>
2020-02-05 20:15:16 +0100

    Let Xcode sign

--- bin/build_vimr.sh
+++ bin/build_vimr.sh
@@ -53,7 +53,8 @@ rm -rf ${build_path}
 
 if [[ ${code_sign} == true ]] ; then
     identity="Developer ID Application: Tae Won Ha (H96Q2NKTQH)"
-    xcodebuild CODE_SIGN_IDENTITY="${identity}" -configuration Release -scheme VimR -workspace VimR.xcworkspace -derivedDataPath ${build_path} clean build
+    entitlements_path=$(realpath NvimView/NvimServer/NvimServer.entitlements)
+    xcodebuild CODE_SIGN_IDENTITY="${identity}" OTHER_CODE_SIGN_FLAGS="--timestamp --options=runtime --entitlements='${entitlements_path}'" -configuration Release -scheme VimR -workspace VimR.xcworkspace -derivedDataPath ${build_path} clean build
     pushd ${build_path}/Build/Products/Release > /dev/null
         codesign --force -s "${identity}" --deep --timestamp --options=runtime VimR.app/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/Autoupdate.app
         codesign --force -s "${identity}" --deep --timestamp --options=runtime VimR.app/Contents/Frameworks/Sparkle.framework/Versions/A
