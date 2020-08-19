aac853f2a854b0bccef0e3ef327bd12c55087b9f
Tae Won Ha <h@taewon.de>
2020-08-19 07:49:24 +0200

    Re-add some demos

--- Commons/Package.swift
+++ Commons/Package.swift
@@ -5,19 +5,12 @@ import PackageDescription
 let package = Package(
   name: "Commons",
   platforms: [.macOS(.v10_13)],
-  products: [
-    .library(
-      name: "Commons",
-      targets: ["Commons"]),
-  ],
+  products: [.library(name: "Commons", targets: ["Commons"])],
   dependencies: [
     .package(url: "https://github.com/Quick/Nimble", .upToNextMinor(from: "8.1.1")),
   ],
   targets: [
-    .target(
-      name: "Commons",
-      dependencies: []
-    ),
+    .target(name: "Commons", dependencies: []),
     .testTarget(
       name: "CommonsTests",
       dependencies: ["Commons", "Nimble"],
