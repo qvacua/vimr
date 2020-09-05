8ea26f6bd288b5903179fc33cf6694c6a95f9326
Tae Won Ha <h@taewon.de>
2020-09-05 12:27:29 +0200

    Refactor slightly

--- bin/build_nvimserver_for_local_dev.sh
+++ bin/build_nvimserver_for_local_dev.sh
@@ -2,22 +2,25 @@
 # Executing this script will replace the download step of pre-built NvimServer.
 set -Eeuo pipefail
 
-readonly clean=${clean:-true}
+readonly clean=${clean:?"true or false: when true, xcodebuild clean for NvimServer and make distclean for libnvim"}
+readonly build_deps=${build_deps:?"true or false: when true, eg libintl will be built"}
+readonly target=${target:?"amr64 or x86_64"}
+
 readonly build_dir_name="build"
 readonly build_dir_path="./${build_dir_name}"
 readonly nvimview_dir_path="./NvimView/Sources/NvimView/"
 
 build_for_local_dev() {
   local -r nvimserver_path="./NvimServer"
+
   pushd ${nvimserver_path} >/dev/null
+    if ${clean} ; then
+      xcodebuild -derivedDataPath ${build_dir_path} -configuration Release -scheme NvimServer clean
+      make distclean
+    fi
 
-  if ${clean} ; then
-    xcodebuild -derivedDataPath ${build_dir_path} -configuration Release -scheme NvimServer clean
-  fi
-
-  build_deps=${clean} ./NvimServer/bin/build_libnvim.sh
-  xcodebuild -derivedDataPath ${build_dir_path} -configuration Release -scheme NvimServer build
-
+    ./NvimServer/bin/build_libnvim.sh
+    xcodebuild -derivedDataPath ${build_dir_path} -configuration Release -scheme NvimServer build
   popd >/dev/null
 
   cp -r "./NvimServer/runtime" ${nvimview_dir_path}
