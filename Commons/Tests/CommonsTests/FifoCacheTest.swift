a59a40e871d7b8013769b17563fbe209e6babf9a
Tae Won Ha <h@taewon.de>
2020-09-18 12:26:49 +0200

    Add simple tests for FifoCache

---
+++ Commons/Tests/CommonsTests/FifoCacheTest.swift
@@ -0,0 +1,33 @@
+/**
+ * Tae Won Ha - http://taewon.de - @hataewon
+ * See LICENSE
+ */
+
+import XCTest
+import Nimble
+
+@testable import Commons
+
+class FifoCacheTest: XCTestCase {
+
+  var fifo: FifoCache<Int, Int>!
+
+  override func setUp() {
+    super.setUp()
+    self.fifo = FifoCache(count: 10, queueQos: .default)
+  }
+
+  func testSimpleGet() {
+    for i in (0...5) { self.fifo.set(i, forKey: i) }
+
+    for i in (0...5) { expect(self.fifo.valueForKey(i)).to(equal(i)) }
+    for i in (6..<10) { expect(self.fifo.valueForKey(i)).to(beNil()) }
+  }
+
+  func testGet() {
+    for i in (0..<(10 * 3)) { self.fifo.set(i, forKey: i) }
+    for i in (20..<30) { expect(self.fifo.valueForKey(i)).to(equal(i)) }
+    expect(self.fifo.valueForKey(19)).to(beNil())
+    expect(self.fifo.valueForKey(30)).to(beNil())
+  }
+}
