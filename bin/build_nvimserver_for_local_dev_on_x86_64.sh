125f6d0ee588acfc57182f753a63278805367544
Tae Won Ha <h@taewon.de>
2020-11-29 11:27:02 +0100

    Add script for local dev on x86_64

---
+++ bin/build_nvimserver_for_local_dev_on_x86_64.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Executing this script will replace the download step of pre-built NvimServer.
+set -Eeuo pipefail
+
+readonly clean=${clean:?"true or false: when true, xcodebuild clean for NvimServer and make distclean for libnvim"}
+readonly build_libnvim=${build_libnvim:?"true or false: when true, libnvim will be built"}
+readonly build_deps=${build_deps:?"true or false: when true, eg libintl will be built"}
+
+readonly build_dir_name="build"
+readonly build_dir_path="./${build_dir_name}"
+readonly nvimview_dir_path="./NvimView/Sources/NvimView/"
+
+build_for_local_dev() {
+  local -r nvimserver_path="./NvimServer"
+
+  pushd ${nvimserver_path} >/dev/null
+    if [[ "${build_libnvim}" == true ]]; then
+      ./NvimServer/bin/build_for_local_dev_on_x86_64.sh
+    fi
+    xcodebuild -derivedDataPath "${build_dir_path}" -configuration Release ARCHS=x86_64 -scheme NvimServer build
+  popd >/dev/null
+  pwd
+  cp "./NvimServer/${build_dir_path}/Build/Products/Release/NvimServer" ${nvimview_dir_path}
+}
+
+
+main() {
+  echo "### Building for local dev"
+  # This script is located in /NvimServer/bin and we have to go to /
+  pushd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null
+
+  build_for_local_dev
+
+  echo "### Built for local dev"
+  popd >/dev/null
+}
+
+main
