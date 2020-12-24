ea92725b7ea4b10b269d8154263644138982c239
Tae Won Ha <h@taewon.de>
2020-12-24 16:32:41 +0100

    Adapt script

--- bin/build_nvimserver_for_local_dev.sh
+++ bin/build_nvimserver_for_local_dev.sh
@@ -5,17 +5,16 @@ declare -x target; target="$(uname -m)"; readonly target
 declare -r -x download_gettext=${download_gettext:-false}
 declare -r -x clean=${clean:-false}
 declare -r -x build_libnvim=${build_libnvim:-true}
-declare -r -x build_dir=${build_dir:-"./build"}
+declare -r -x build_dir=${build_dir:-"./.build"}
 
 main() {
   pushd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null
 
   pushd "./NvimServer"
     ./NvimServer/bin/build_nvimserver.sh
-    cp ./build/Build/Products/Release/NvimServer ../NvimView/Sources/NvimView/Resources
+    cp ./.build/release/NvimServer ../NvimView/Sources/NvimView/Resources
     cp -r ./runtime ../NvimView/Sources/NvimView/Resources
     cp ../NvimView/Sources/NvimView/Resources/com.qvacua.NvimView.vim ../NvimView/Sources/NvimView/Resources/runtime/plugin
-
   popd >/dev/null
 
   popd >/dev/null
