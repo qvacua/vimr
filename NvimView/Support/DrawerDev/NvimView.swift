d1787e23da35347c14edaf6c636af40bd9b1c06f
Tae Won Ha <h@taewon.de>
2020-12-24 22:22:49 +0100

    Fix support apps

---
+++ NvimView/Support/DrawerDev/NvimView.swift
@@ -0,0 +1,13 @@
+/**
+ * Tae Won Ha - http://taewon.de - @hataewon
+ * See LICENSE
+ */
+
+import Cocoa
+
+// Dummy NvimView class for FontUtils.
+class NvimView {
+  static let defaultFont = NSFont.userFixedPitchFont(ofSize: 12)!
+  static let minFontSize = CGFloat(9)
+  static let maxFontSize = CGFloat(128)
+}
