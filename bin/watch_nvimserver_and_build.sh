c2788b437c6054680deb89c9a9665af72d4a33f5
Tae Won Ha <h@taewon.de>
2020-12-24 10:44:24 +0100

    Fix script

--- bin/watch_nvimserver_and_build.sh
+++ bin/watch_nvimserver_and_build.sh
@@ -3,7 +3,7 @@ set -Eeuo pipefail
 
 main() {
   pushd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null
-    find NvimServer/Sources NvimServerTypes | grep -E '(\.h$|\.c$)' | entr -c ./bin/build_nvimserver_for_local_dev.sh
+    find NvimServer/NvimServer/Sources NvimServer/NvimServerTypes | grep -E '(\.h$|\.c$)' | entr -c ./bin/build_nvimserver_for_local_dev.sh
   popd >/dev/null
 }
 
