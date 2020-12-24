ed84e6ac491cf925aaa7d6c3118ac7002edcc975
Tae Won Ha <h@taewon.de>
2020-12-24 10:56:46 +0100

    Extend script slightly

--- bin/watch_nvimserver_and_build.sh
+++ bin/watch_nvimserver_and_build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+readonly clean_initial_build=${clean_inital_build:-false}
+
+clean_build() {
+  local -r -x download_gettext=true
+  local -r -x clean=true
+  local -r -x build_libnvim=true
+  ./bin/build_nvimserver_for_local_dev.sh
+}
+
 main() {
   pushd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null
+    if [[ "${clean_initial_build}" == true ]]; then
+      clean_build
+    fi
+
     find NvimServer/NvimServer/Sources NvimServer/NvimServerTypes | grep -E '(\.h$|\.c$)' | entr -c ./bin/build_nvimserver_for_local_dev.sh
   popd >/dev/null
 }
