eb763ea6d17606c0e9835ea563c9b020c930d099
Tae Won Ha <h@taewon.de>
2022-11-20 11:26:06 +0100

    Use the new notarytool

--- bin/notarize_vimr.sh
+++ bin/notarize_vimr.sh
@@ -8,20 +8,11 @@ main() {
     echo "### Notarizing"
     ditto -c -k --keepParent VimR.app VimR.app.zip
 
-    echo "#### Uploading"
-    local -x request_uuid
-    request_uuid=$(xcrun altool --notarize-app --primary-bundle-id "com.qvacua.VimR" --username "hataewon@gmail.com" --password "@keychain:dev-notar" --file VimR.app.zip | grep RequestUUID | sed -E 's/.* = (.*)/\1/')
-    readonly request_uuid
-
-    echo "#### Uploaded"
-    echo "Use 'xcrun altool --notarization-history 0 -u hataewon@gmail.com -p @keychain:dev-notar' or"
-    echo "'xcrun altool --notarization-info ${request_uuid} -u hataewon@gmail.com -p @keychain:dev-notar'"
+    echo "#### Notarizing"
+    xcrun notarytool submit VimR.app.zip \
+      --keychain-profile  "apple-dev-notar" \
+      --wait
   popd >/dev/null
-
-  pushd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null
-    echo "#### Waiting for notarization ${request_uuid} to finish"
-    ./bin/wait_for_notarization.py
-  popd </dev/null
 
   pushd "${vimr_app_path}/.." >/dev/null
     xcrun stapler staple VimR.app
